5136fcc8e6b19a4f19a108833512f46ad2ca556b
SvitlanaOseichuk <141953748+SvitlanaOseichuk@users.noreply.github.com>
2024-02-02 00:52:51 +0200

    add styles, done with js

--- src/main.js
+++ src/main.js
@@ -8,60 +8,78 @@ const API_KEY = "42149629-e1e8d2a9238f015bc3d9ce966";
 
 const refs = {
     form: document.getElementById("form"),
-    resultContainer: document.getElementById("result-container")
+    resultContainer: document.getElementById("result-container"),
+    loadingIndicator: document.querySelector(".loader")
 };
 
 refs.form.addEventListener("submit", handleSearch);  //лісенер на форму
+
+function clearResultContainer() {
+    refs.resultContainer.innerHTML = "";//div
+}
+
+function showLoadingIndicator() {
+    refs.loadingIndicator.style.display = "block";//on lo
+}
+
+function hideLoadingIndicator() {
+    refs.loadingIndicator.style.display = "none";//off lo
+}
+
 
 function handleSearch(event) {  // отримуємо дані з інпуту
     event.preventDefault();
     const form = event.currentTarget;
     const requestForSearch = form.elements.search.value;
 
-    if (form.elements.search.value.length === 0){
+    clearResultContainer();
+    showLoadingIndicator();//
+
+
+    if (requestForSearch.trim().length === 0){
             iziToast.show({
-                message: "Sorry, there are no images matching your search query. Please, try again!",
+                message: "Please, try again!",
                 color: "red"
-            });  
+            });
+            hideLoadingIndicator(); //крутялка
             return;
         }
+    clearResultContainer();// контейнер
+    showLoadingIndicator()// крутялка
 
 
-        showLoadingIndicator(); // Відобразити індикатор завантаження перед запитом
+    searchPictures(requestForSearch).then((data) =>{
 
-    searchPictures(requestForSearch)
-    .then((data) =>{
         const pictures = data.hits;
 
-        let markup = ""
-        for (const pic of pictures) {
-            markup +=createPicturesMarkup(pic);
-            // console.log(pic)
-        }
+        if (pictures.length === 0) {
+            iziToast.show({
+                message: "Sorry, there are no images matching your search query. Please try again!"
+            });
+        } else {
+            let markup = ""
+            for (const pic of pictures) {
+                markup +=createPicturesMarkup(pic);
+            }    
         refs.resultContainer.innerHTML = markup;
-        
+        }
     })
-    
+    .catch(error => {
+        console.error('Error:', error);
+    })
     .finally(() => {
-        // Приховати індикатор завантаження після завершення запиту
-        hideLoadingIndicator();
+        hideLoadingIndicator(); //крутялка
         form.reset();
+        
     });
 }
 
-function showLoadingIndicator() {
-    document.getElementById("loading-indicator").style.display = "block";
-}
-
-function hideLoadingIndicator() {
-    document.getElementById("loading-indicator").style.display = "none";
-}
 
 function searchPictures(requestForSearch) {
-    return fetch(`${URL}?key=${API_KEY}&q=${requestForSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12`).then(
+    return fetch(`${URL}/?key=${API_KEY}&q=${requestForSearch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12`).then(
         (res) => {
             if (!res.ok){
-                throw new Error(`Network response was not ok: ${response.statusText}`);
+                throw new Error(`Network response was not ok: ${res.statusText}`);
             }
             return res.json();
         }
@@ -69,75 +87,25 @@ function searchPictures(requestForSearch) {
 }
 
 
-function createPicturesMarkup({likes, views, comments, downloads, webformatURL, largeImageURL}) {
-    return `<li>
+function createPicturesMarkup({likes, views, comments, downloads, tags, webformatURL, largeImageURL}) {
+    return `<li class="image-container">
         <a class="gallery-link" href="${largeImageURL}">
          <img
            class="gallery-image"
            src="${webformatURL}"
+           alt="${tags}"
          />
         </a>
-        <p>like ${likes}</p>
-        <p>viewsc ${views}</p>
-        <p>comments ${comments}</p>
-        <p>downloads ${downloads}</p>
+        <div class="overlay">
+        <p>likes<br>${likes}</p>
+        <p>views<br>${views}</p>
+        <p>comments<br>${comments}</p>
+        <p>downloads<br>${downloads}</p></div>
     </li>
 `
 }
 
-    
-
-
-
-
-
-
-
-
-// let gallery = new SimpleLightbox('.gallery a');
-// gallery.on('show.simplelightbox', function () {
-// 	// Do something…
-// });
-
-// gallery.on('error.simplelightbox', function (e) {
-// 	console.log(e); // Some usefull information
-// });
-
-// galery
-
-
-
-// const images = [
-//     {
-//       preview:
-//         "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-//       original:
-//         "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-//       description: "Hokkaido Flower",
-//     },
-// ];
-
-// const gallery = document.querySelector(".gallery"); 
-
-
-// gallery.innerHTML = createMarkup(images);
-
-// function createMarkup(images){
-//   return images.map(({preview, original, description}) => 
-//   `<li class="gallery-item">
-//   <a class="gallery-link" href="${original}">
-//     <im
-//       class="gallery-image"
-//       src="${preview}"
-//       alt="${description}"
-//     />
-//   </a>
-//   </li>`
-//   )
-// .join ("")
-// }
-
-// const lightbox = new SimpleLightbox(".gallery a", {  
-//   captionsData: "alt", 
-//   captionDelay: 250,
-// }
+const lightbox = new SimpleLightbox(".gallery a", {  
+    captionsData: "alt", 
+    captionDelay: 250,
+  });
